bc28f5a036960d66fc928c79f7325635e38d5044
Kyounghyun Ryu <eva700ca@gmail.com>
2015-02-20 23:33:00 -0500

    project page update

--- Portfolio-Mobile/Scripts/_references.js
+++ Portfolio-Mobile/Scripts/_references.js
@@ -2,3 +2,7 @@
 /// <reference path="jquery-1.8.0.js" />
 /// <reference path="jquery.mobile-1.4.5.js" />
 /// <reference path="mobile.js" />
+/// <reference path="foundation.min.js" />
+/// <reference path="jquery.js" />
+/// <reference path="flick.js" />
+/// <reference path="common.js" />
--- Portfolio-Mobile/Scripts/mobile.js
+++ Portfolio-Mobile/Scripts/mobile.js
@@ -1,3 +1,8 @@
 ﻿function init() {
     console.log("mobile java script start")
 }
+/* Instantiate the popup on DOMReady, and enhance its contents */
+$(function popup() {
+    console.log("popup-outside-page script start")
+    $("#popup-outside-page").enhanceWithin().popup();
+});
